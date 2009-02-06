f5a869b16f1690150b74963c50d2ba35a2acb339
Ozan Çağlayan <ozan@pardus.org.tr>
2009-02-06 07:06:03 +0000

    add tarballer

---
+++ tarball.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf command-not-found-*
+
+svn export command-not-found command-not-found-$1
+mkdir command-not-found-$1/data
+
+cd command-not-found-$1/data
+sudo python ../src/generate.py
+cd
+
+tar cvjf command-not-found-$1.tar.bz2 command-not-found-$1
+
+sha1sum command-not-found-$1.tar.bz2
+scp command-not-found-$1.tar.bz2 ozan@cekirdek.pardus.org.tr:public_html/dist/sources
+
